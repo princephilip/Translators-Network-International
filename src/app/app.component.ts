8d74c800c413ffb6e557323a35d79b2313b75f74
Philip Okafor <o.philip@cityu.gr>
2019-01-04 10:01:17 +0200

    Update app-component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {MDBSpinningPreloader } from 'ng-uikit-pro-standard';
+import { AuthService } from './_services';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+
+export class AppComponent implements OnInit{
+
+  constructor(private mdbSpinningPreloader: MDBSpinningPreloader,
+              public auth: AuthService){
+                console.log(auth.loggedIn())
+              }
+
   title = 'tni';
+
+  ngOnInit() {
+    this.mdbSpinningPreloader.stop();
+  }
+
 }
